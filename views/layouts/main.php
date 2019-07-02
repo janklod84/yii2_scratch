bea1eeba8848613edef322b8dbb2a2cc1afb3264
janklod84 <jeanyao34@yahoo.com>
2019-07-02 23:16:55 +0500

    add bootstrap and navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,10 +1,33 @@
+<?php 
+use yii\bootstrap\NavBar;
+
+?>
 <!DOCTYPE html>
+<?php $this->beginPage(); ?>
 <html>
 <head>
-	<title>Learn</title>
+	<title>Learn Yii</title>
+	<?php $this->head(); ?>
 </head>
 <body>
-   <?= $content ?>
-   
+<?php  $this->beginBody() ?>
+<?php 
+   NavBar::begin([
+    'brandLabel' => 'MyBrand', // Brand
+    'brandUrl'   => Yii::$app->homeUrl, // base URL
+    'options' => [
+       'class' => 'navbar-default navbar-fixed-top'
+    ]
+ ]);
+
+  NavBar::end();
+?>
+ 
+  <div class="container" style="margin-top: 70px;">
+  	 <?= $content ?>
+  </div>
+
+<?php $this->endBody(); ?>
 </body>
 </html>
+<?php $this->endPage(); ?>
