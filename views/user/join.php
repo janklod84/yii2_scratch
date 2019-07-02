acf32610b035bdff3c5daa2599e8f67a0cdb3d03
janklod84 <jeanyao34@yahoo.com>
2019-07-03 02:30:09 +0500

    describe join and login pages

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,12 @@
-<h1>Join Us</h1>
+<div class="panel panel-danger">
+	<div class="panel-heading">
+	  <h1>Join Us</h1>
+    </div>
+    <div class="panel-body">
+    	name<br> 
+    	email<br>
+    	password<br>
+    	confirm<br> 
+    	[Create]
+    </div>
+</div>
--- views/user/login.php
+++ views/user/login.php
@@ -1,1 +1,10 @@
-<h1>Log in</h1>
+<div class="panel panel-info">
+	<div class="panel-heading">
+	  <h1>Log in</h1>
+    </div>
+    <div class="panel-body">
+    	email<br>
+    	password<br>
+    	[Enter]
+    </div>
+</div>
