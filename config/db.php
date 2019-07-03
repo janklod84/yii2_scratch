d999c8daaa18d6b0f823718add48759030137c7e
janklod84 <jeanyao34@yahoo.com>
2019-07-03 05:21:14 +0500

    Database configuration , module and migration

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Database configuration
+*/
+return [
+ 'class'    => '\yii\db\Connection',
+ 'dsn'      => 'mysql:host=localhost;dbname=yii_scratch;',
+ 'username' => 'root',
+ 'password' => ''
+];
